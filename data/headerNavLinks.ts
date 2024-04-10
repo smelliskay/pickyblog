8777ba54224ddfc0c5b0e88553e117161acb6514
smelliskay <elliskay2000@gmail.com>
2024-04-10 16:14:37 -0400

    prettier write

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,10 +1,10 @@
 const headerNavLinks = [
-  { href: '/', title: 'Home' },
-  { href: 'www.google.com', title: 'Pikmin'},
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
-]
+  { href: "/", title: "Home" },
+  { href: "www.google.com", title: "Pikmin" },
+  { href: "/blog", title: "Blog" },
+  { href: "/tags", title: "Tags" },
+  { href: "/projects", title: "Projects" },
+  { href: "/about", title: "About" },
+];
 
-export default headerNavLinks
+export default headerNavLinks;
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,25 +1,25 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Smelly.dev',
-  author: 'Smelly guy',
-  headerTitle: 'Smelly.dev',
-  description: 'A blog for tech topics',
-  language: 'en-us',
-  theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/smelliskay/pickyblog',
-  siteLogo: '/static/images/logo.png',
-  socialBanner: '/static/images/twitter-card.png',
-  mastodon: '',
-  email: 'elliskay2000@gmail.com',
-  github: 'https://github.com/smelliskay',
-  twitter: '',
-  facebook: '',
-  youtube: '',
-  linkedin: 'https://www.linkedin.com/in/ellis-kay-768494208',
-  threads: '',
-  instagram: '',
-  locale: 'en-US',
+  title: "Smelly.dev",
+  author: "Smelly guy",
+  headerTitle: "Smelly.dev",
+  description: "A blog for tech topics",
+  language: "en-us",
+  theme: "system", // system, dark or light
+  siteUrl: "https://tailwind-nextjs-starter-blog.vercel.app",
+  siteRepo: "https://github.com/smelliskay/pickyblog",
+  siteLogo: "/static/images/logo.png",
+  socialBanner: "/static/images/twitter-card.png",
+  mastodon: "",
+  email: "elliskay2000@gmail.com",
+  github: "https://github.com/smelliskay",
+  twitter: "",
+  facebook: "",
+  youtube: "",
+  linkedin: "https://www.linkedin.com/in/ellis-kay-768494208",
+  threads: "",
+  instagram: "",
+  locale: "en-US",
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
@@ -45,14 +45,14 @@ const siteMetadata = {
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
     // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
+    provider: "buttondown",
   },
   comments: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // Select a provider and use the environment variables associated to it
     // https://vercel.com/docs/environment-variables
-    provider: 'giscus', // supported providers: giscus, utterances, disqus
+    provider: "giscus", // supported providers: giscus, utterances, disqus
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
@@ -60,27 +60,27 @@ const siteMetadata = {
       repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
       category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
       categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
-      mapping: 'pathname', // supported options: pathname, url, title
-      reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
+      mapping: "pathname", // supported options: pathname, url, title
+      reactions: "1", // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
-      metadata: '0',
+      metadata: "0",
       // theme example: light, dark, dark_dimmed, dark_high_contrast
       // transparent_dark, preferred_color_scheme, custom
-      theme: 'light',
+      theme: "light",
       // theme when dark mode
-      darkTheme: 'transparent_dark',
+      darkTheme: "transparent_dark",
       // If the theme option above is set to 'custom`
       // please provide a link below to your custom theme css file.
       // example: https://giscus.app/themes/custom_example.css
-      themeURL: '',
+      themeURL: "",
       // This corresponds to the `data-lang="en"` in giscus's configurations
-      lang: 'en',
+      lang: "en",
     },
   },
   search: {
-    provider: 'kbar', // kbar or algolia
+    provider: "kbar", // kbar or algolia
     kbarConfig: {
-      searchDocumentsPath: 'search.json', // path to load documents to search
+      searchDocumentsPath: "search.json", // path to load documents to search
     },
     // provider: 'algolia',
     // algoliaConfig: {
@@ -91,6 +91,6 @@ const siteMetadata = {
     //   indexName: 'docsearch',
     // },
   },
-}
+};
 
-module.exports = siteMetadata
+module.exports = siteMetadata;
